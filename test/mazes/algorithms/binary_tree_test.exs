eb0b26b2fab0a5f49830a631b5a874a7d34b059a
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-29 13:03:35 +0000

    Use assert_value with fixed random seed to test algorithms

--- test/mazes/algorithms/binary_tree_test.exs
+++ test/mazes/algorithms/binary_tree_test.exs
@@ -3,6 +3,7 @@
 
 defmodule Mazes.Algorithms.BinaryTreeTest do
   use ExUnit.Case, async: true
+  import AssertValue
 
   alias Mazes.Algorithms.BinaryTree
   alias Mazes.Grid
@@ -10,24 +11,23 @@ defmodule Mazes.Algorithms.BinaryTreeTest do
   doctest BinaryTree
 
   describe "Mazes.BinaryTree.on/1" do
-    test "links all cells on the northern border" do
-      grid = Grid.new(3, 3) |> BinaryTree.on()
-      assert Grid.linked_east?(grid, {2, 0})
-      assert Grid.linked_east?(grid, {2, 1})
-    end
+    test "generates a maze" do
+      :rand.seed(:exrop, {101, 102, 103})
+      grid = BinaryTree.on(Grid.new(5, 5))
 
-    test "links all cells on the eastern border" do
-      grid = Grid.new(3, 3) |> BinaryTree.on()
-      assert Grid.linked_north?(grid, {0, 2})
-      assert Grid.linked_north?(grid, {1, 2})
-    end
-
-    test "links all other cells to either the north or east" do
-      grid = Grid.new(3, 3) |> BinaryTree.on()
-
-      for row <- [0, 1], column <- [0, 1] do
-        assert Grid.linked_north?(grid, {row, column}) or Grid.linked_east?(grid, {row, column})
-      end
+      assert_value inspect(grid) == """
+                   +---+---+---+---+---+
+                   |                   |
+                   +---+---+   +   +   +
+                   |           |   |   |
+                   +   +---+---+   +   +
+                   |   |           |   |
+                   +   +---+---+---+   +
+                   |   |               |
+                   +   +   +---+   +   +
+                   |   |   |       |   |
+                   +---+---+---+---+---+
+                   """
     end
   end
 end
--- test/mazes/algorithms/sidewinder_test.exs
+++ test/mazes/algorithms/sidewinder_test.exs
@@ -3,6 +3,7 @@
 
 defmodule Mazes.Algorithms.SidewinderTest do
   use ExUnit.Case, async: true
+  import AssertValue
 
   alias Mazes.Algorithms.Sidewinder
   alias Mazes.Grid
@@ -10,30 +11,23 @@ defmodule Mazes.Algorithms.SidewinderTest do
   doctest Sidewinder
 
   describe "Mazes.Sidewinder.on/1" do
-    test "links all cells on the northern border" do
-      grid = Grid.new(3, 3) |> Sidewinder.on()
-      assert Grid.linked_east?(grid, {2, 0})
-      assert Grid.linked_east?(grid, {2, 1})
-    end
+    test "generates a maze" do
+      :rand.seed(:exrop, {101, 102, 103})
+      grid = Sidewinder.on(Grid.new(5, 5))
 
-    test "creates one northward link from each horizontal run of cells on other rows" do
-      grid = Grid.new(4, 4) |> Sidewinder.on()
-
-      for row <- 0..2, column <- 0..3 do
-        {row, column}
-      end
-      |> Enum.chunk_while(
-        [],
-        fn cell, run ->
-          if Grid.linked_east?(grid, cell),
-            do: {:cont, [cell | run]},
-            else: {:cont, Enum.reverse([cell | run]), []}
-        end,
-        fn run -> {:cont, Enum.reverse(run)} end
-      )
-      |> Enum.each(fn run ->
-        assert Enum.count(run, &Grid.linked_north?(grid, &1)) == 1
-      end)
+      assert_value inspect(grid) == """
+                   +---+---+---+---+---+
+                   |                   |
+                   +---+   +---+   +---+
+                   |           |       |
+                   +   +---+   +---+   +
+                   |   |           |   |
+                   +   +---+---+---+   +
+                   |   |               |
+                   +   +   +   +---+   +
+                   |   |   |       |   |
+                   +---+---+---+---+---+
+                   """
     end
   end
 end
